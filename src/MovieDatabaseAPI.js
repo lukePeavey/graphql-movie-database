8d5514626f2a845018cc99256b2af80c8d9aa88b
Luke Peavey <1williampeavey@gmail.com>
2018-12-19 15:40:08 -0500

    Add methods to the RestDataSource
    - Add methods to handle requests to search/discover endpoints
    
    - Add a custom get method to apply transforms to response data
      instead of repeating them in every method
    
    - Add utilities to handle converting arguments and response data
      to/from camelCase

--- src/MovieDatabaseAPI.js
+++ src/MovieDatabaseAPI.js
@@ -1,5 +1,5 @@
 const { RESTDataSource } = require('apollo-datasource-rest')
-const camelCaseKeys = require('camelcase-keys')
+const { camelCaseKeys, deCamelCaseArgs } = require('./utils/camelCase')
 
 /**
  * A data source to connect to the TMDB rest API
@@ -7,11 +7,7 @@ const camelCaseKeys = require('camelcase-keys')
 class MovieDatabaseAPI extends RESTDataSource {
   constructor() {
     super()
-    this.baseURL = `https://api.themoviedb.org/3/`
-  }
-
-  camelCaseKeys(input, opts = {}) {
-    return camelCaseKeys(input, { deep: true, ...opts })
+    this.baseURL = `https://api.themoviedb.org/3`
   }
 
   willSendRequest(request) {
@@ -19,21 +15,105 @@ class MovieDatabaseAPI extends RESTDataSource {
     request.params.set('api_key', process.env.TMDB_API_KEY)
   }
 
+  /**
+   * Handles get requests
+   * This overrides the `get` method on the parent class to perform some general
+   * transformations to the response data that are common to all get methods.
+   * @see RESTDataSource.prototype.get
+   */
+  async get(path, params, init) {
+    const response = camelCaseKeys(await super.get(path, params, init))
+    // For single item queries => `Item`
+    if (!response.results) return response
+    // For search/discover queries => `{ results: [Item], meta: QueryMeta }`
+    const { results, ...meta } = response
+    return { results, meta }
+  }
+
   /** Get details about a single Movie by ID */
-  async getMovie(id) {
-    const data = await this.get(`movie/${id}`)
-    return this.camelCaseKeys(data)
+  async getMovieById(id) {
+    const params = { append_to_response: 'credits,images,reviews' }
+    return this.get(`/movie/${id}`, params)
   }
+
   /** Get details about a single tv show by ID */
-  async getShow(id) {
-    const data = await this.get(`tv/${id}`)
-    return this.camelCaseKeys(data)
+  async getShowById(id) {
+    return this.get(`/tv/${id}`, {
+      append_to_response: 'credits,images,reviews,seasons'
+    })
   }
+
   /** Get details about a single person by ID */
-  async getPerson(id) {
-    const data = await this.get(`person/${id}`)
-    return this.camelCaseKeys(data)
+  async getPersonById(id) {
+    return this.get(`/person/${id}`, {
+      append_to_response: 'combined_credits,images'
+    })
+  }
+  /** Get a genre by ID */
+  async getGenreById(type, id) {
+    const { genres } = await this.getGenreList(type)
+    return genres.find(item => item.id === id)
+  }
+
+  /** Get a single season of a TV show, including all episodes */
+  async getSeason(showId, seasonNumber) {
+    return this.get(`/tv/${showId}/season/${seasonNumber}`)
+  }
+
+  /**
+   * Get the list of official genres for a specific media type
+   * @param {'movie' | 'tv'} type the type of genres to get
+   */
+  async getGenreList(type) {
+    return this.get(`genre/${type}/list`)
+  }
+
+  /**
+   * Find movies/shows using the `/discover/` endpoint
+   *
+   * @todo
+   * Caching doesn't seem to be working on requests to `/discover/tv`.
+   * If you run the `movies` query multiple times with the same args, it
+   * uses the cached response from `/discover/movie` after the first
+   * query. However, with the `shows` query, it looks like its making a
+   * new request to `/discover/tv` on every query.
+   * 1. Lean more about cache configuration options for 'RestDataSource.'
+   * 2. How to get more details about the cache
+   *
+   * @param {'movie' | 'tv'} endpoint
+   * @param {Object} params
+   * @param {Object} params.filter supports all filtering options that are
+   * available on the `/discover/movie` endpoint. The format of the
+   * argument names is slightly different, see schema for details
+   * @param {String} params.sortBy Supports all values available for the
+   * `sort_by` parameter on the `/discover/movie/` endpoint. See
+   * schema for details
+   * @param {number} params.page Must be an Int <= 1000
+   */
+  async discover(endpoint, params = {}) {
+    const { filter = {}, ...rest } = params
+    return this.get(`/discover${endpoint}`, {
+      ...deCamelCaseArgs(filter),
+      ...deCamelCaseArgs(rest),
+      cacheControl: { ttl: 1000 }
+    })
+  }
+
+  /**
+   * Search for items matching the provided query.
+   * @todo Add support for other search endpoints like `company` and `keyword`
+   *
+   * @param {Object} params
+   * @param {'movie' | 'tv' | 'person' | 'company' | 'multi'} params.type
+   * @param {String} params.query the query string to search for
+   * @param {number} params.page the pagination offset. Must be Int <= 1000
+   */
+  async search({ type = 'multi', ...params }) {
+    let { results, meta } = await this.get(`/search/${type}`, params)
+    if (type !== 'multi') {
+      results = results.map(item => ({ mediaType: type, ...item }))
+    }
+    return { results, meta }
   }
 }
-
 module.exports = MovieDatabaseAPI
---
+++ src/utils/camelCase.js
@@ -0,0 +1,40 @@
+const { RESTDataSource } = require('apollo-datasource-rest')
+const camelCaseKeys = require('camelcase-keys')
+const isPlainObject = require('lodash/isPlainObject')
+const isString = require('lodash/isString')
+const snakeCase = require('lodash/snakeCase')
+
+module.exports.camelCaseKeys = function(input, opts = {}) {
+  return camelCaseKeys(input, { deep: true, ...opts })
+}
+
+/**
+ * Convert a string to snake_case, with special treatment for suffixed
+ * strings, see example
+ * @example
+ * deCamelCase("releaseDate_DESC")
+ * // => "release_date.desc"
+ *
+ * @param {*} input the value to transform
+ */
+function deCamelCase(input) {
+  if (!isString(input)) return input
+  return input.split('_').map(snakeCase).join('.') // prettier-ignore
+}
+
+/**
+ * Recursively transform arguments to the format used by TMDB REST endpoints.
+ * Transforms both keys and values.
+ * @example
+ * deCamelCaseArgs({ sortBy: "releaseDate_DESC" })
+ * // => { sort_by: "release_date.desc" }
+ *
+ * @param {Object} args
+ */
+module.exports.deCamelCaseArgs = function deCamelCaseArgs(args) {
+  return Object.keys(args).reduce((r, k) => {
+    let value = isPlainObject(args[k]) ? deCamelCaseArgs(args[k]) : args[k]
+    if (k === 'sortBy') value = deCamelCase(value)
+    return { ...r, [deCamelCase(k)]: value }
+  }, {})
+}

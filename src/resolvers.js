7c8b494466dd171db6649f518cd76ced372d9c33
Luke Peavey <1williampeavey@gmail.com>
2018-12-25 03:01:36 -0500

    Extend cast and crew fields

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,8 @@
 const transforms = require('./utils/transforms')
 const capitalize = require('lodash/capitalize')
+const camelCase = require('lodash/camelCase')
+
+// @TODO Refactor to reduce code repetition!
 
 module.exports = {
   Query: {
@@ -47,16 +50,23 @@ module.exports = {
   },
   Movie: {
     mediaType: () => 'movie',
-    // If the response doesn't already include the `credits` property, make an
-    // API request to `/movie/${id}` to get the credits.
-    credits: async ({ credits, id }, _, { dataSources }) => {
-      if (credits) return credits
-      const data = await dataSources.api.getMovie(id)
-      return data.credits
+    // Get cast credits for Movie object
+    cast: async ({ credits, id }, { first }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getMovie(id))['credits']
+      return !first ? credits.cast : credits.cast.slice(0, first)
     },
-    videos: async ({ videos, id }, _, { dataSources }) => {
+    // Get crew credits for Movie object
+    crew: async ({ credits, id }, { departments }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getMovie(id))['credits']
+      if (!departments) return credits.crew
+      return credits.crew.filter(item => {
+        return departments.includes(camelCase(item.department))
+      })
+    },
+    videos: async ({ videos, id }, { type }, { dataSources }) => {
       if (!videos) videos = (await dataSources.api.getMovie(id))['videos']
-      return videos.results
+      if (!type) return videos.results
+      return videos.results.filter(item => camelCase(item.type) === type)
     },
     images: async ({ images, id }, { dataSources }) => {
       return images || (await dataSources.api.getMovie(id))['images']
@@ -79,16 +89,23 @@ module.exports = {
     mediaType: () => 'tv',
     title: ({ name }) => name, // make consistent with Movie
     originalTitle: ({ originalName }) => originalName,
-    // If the response doesn't already include the `credits` property, make an
-    // request to `/tv/${id}` to get the credits.
-    credits: async function({ credits, id }, _, { dataSources }) {
-      if (credits) return credits
-      const data = await dataSources.api.getShow(id)
-      return data.credits
+    // Get cast credits for TV Show
+    cast: async ({ credits, id }, { first }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getShow(id))['credits']
+      return !first ? credits.cast : credits.cast.slice(0, first)
     },
-    videos: async ({ id, ...obj }, _, { dataSources }) => {
-      const videos = obj.videos || (await dataSources.api.getShow(id))['videos']
-      return videos.results
+    // Get crew credits for TV Show
+    crew: async ({ credits, id }, { departments }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getShow(id))['credits']
+      if (!departments) return credits.crew
+      return credits.crew.filter(item => {
+        return departments.includes(camelCase(item.department))
+      })
+    },
+    videos: async ({ videos, id }, { type }, { dataSources }) => {
+      videos = videos || (await dataSources.api.getShow(id))['videos']
+      if (!type) return videos.results
+      return videos.results.filter(item => camelCase(item.type) === type)
     },
     images: async ({ id, images }, { dataSources }) => {
       return images || (await dataSources.api.getShow(id))['images']
@@ -100,24 +117,46 @@ module.exports = {
     genres: async ({ genreIds, genres }, _, { dataSources }) => {
       return genres || transforms.getGenres('tv', genreIds, dataSources)
     },
+    // Get all seasons of a show
     // @todo Figure out a better way to handle querying seasons & episodes
-    seasons: ({ id, seasons }) => {
+    seasons: async ({ seasons, id }, _, { dataSources }) => {
+      seasons = seasons || (await dataSources.api.getShow(id))['seasons']
       // Pass down the `showId` prop to the `season` field. This allows it to
       // make an API request to `/tv/${showId}/season/${seasonNumber}` to get
       // episodes when the `episodes` field is present in the query.
       return seasons.map(season => ({ showId: id, ...season }))
+    },
+    // Gets a single season of the show
+    season: async ({ id: showId }, { seasonNumber }, { dataSources }) => {
+      const season = await dataSources.api.getSeason({ showId, seasonNumber })
+      // Pass down the `showId` prop to the `season` field. This allows it to
+      // make an API request to `/tv/${showId}/season/${seasonNumber}` to get
+      // episodes when the `episodes` field is present in the query.
+      return { showId, ...season }
     }
   },
   Season: {
     title: ({ name }) => name, // make consistent with Movie
-    credits: async ({ credits, ...obj }, _, { dataSources }) => {
-      if (credits) return credits
-      const data = await dataSources.api.getSeason(obj)
-      return data.credits
+    // Get the cast credits for the season
+    // @note this requires an API call to `/tv/${id}/season/${seasonNumber}`
+    cast: async ({ credits, ...obj }, { first }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getSeason(obj))['credits']
+      return !first ? credits.cast : credits.cast.slice(0, first)
     },
-    videos: async ({ videos, ...obj }, _, { dataSources }) => {
+    // Get the crew credits for the season
+    // @note this requires an API call to `/tv/${id}/season/${seasonNumber}`
+    crew: async ({ credits, ...obj }, { departments }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getSeason(obj))['credits']
+      if (!departments) return credits.crew
+      return credits.crew.filter(item => {
+        return departments.includes(camelCase(item.department))
+      })
+    },
+    // Get the crew credits for the season
+    videos: async ({ videos, ...obj }, { type }, { dataSources }) => {
       if (!videos) videos = (await dataSources.api.getSeason(obj))['videos']
-      return videos.results
+      if (!type) return videos.results
+      return videos.results.filter(item => camelCase(item.type) === type)
     },
     images: async ({ images, ...obj }, _, { dataSources }) => {
       return images || (await dataSources.api.getSeason(obj))['images']
@@ -125,9 +164,15 @@ module.exports = {
     reviews: async ({ reviews, ...obj }, _, { dataSources }) => {
       return reviews || (await dataSources.api.getSeason(obj))['reviews']
     },
+    // Gets all episodes of the season
+    // @todo figure out better way to handle querying episodes
     episodes: async ({ showId, seasonNumber }, _, { dataSources }) => {
       const data = await dataSources.api.getSeason({ showId, seasonNumber })
       return data.episodes
+    },
+    // Gets a single episode of the season
+    episode: async (obj, { episodeNumber }, { dataSources }) => {
+      return await dataSources.api.getEpisode({ ...obj, episodeNumber })
     }
   },
   Episode: {
@@ -135,16 +180,21 @@ module.exports = {
     images: async ({ images, ...obj }, _, { dataSources }) => {
       return images || (await dataSources.api.getEpisode(obj))['images']
     },
-    videos: async ({ videos, ...obj }, _, { dataSources }) => {
+    videos: async ({ videos, ...obj }, { type }, { dataSources }) => {
       if (!videos) videos = (await dataSources.api.getEpisode(obj))['videos']
-      return videos.results
+      if (!type) return videos.results
+      return videos.results.filter(item => camelCase(item.type) === type)
     },
-    cast: async ({ credits, ...obj }, _, { dataSources }) => {
-      credits = credits || (await dataSources.api.getEpisode(obj))['credits']
-      return credits.cast
+    cast: async ({ credits, ...obj }, { first }, { dataSources }) => {
+      credits = credits || (await dataSources.api.getMovie(obj))['credits']
+      return !first ? credits.cast : credits.cast.slice(0, first)
     },
-    crew: async ({ crew, ...obj }, _, { dataSources }) => {
-      return crew || (await dataSources.api.getEpisode(obj))['crew']
+    crew: async ({ crew, ...obj }, { departments }, { dataSources }) => {
+      crew = crew || (await dataSources.api.getSeason(obj))['credits']
+      if (!departments) return crew
+      return crew.filter(item => {
+        return departments.includes(camelCase(item.department))
+      })
     }
   },
   Person: {
